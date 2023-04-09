1cda0f8ad4f3a99c74e601af6e53a0821f478492
huns <j840616@gmail.com>
2023-04-10 01:44:18 +0900

    15

--- slidigital/randing/urls.py
+++ slidigital/randing/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('results/', views.results, name='results'),
-    path('statics/', views.statics, name='statics'),
+    # path('statics/', views.statics, name='statics'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
