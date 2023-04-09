bb7e10a2b0d286fd4c5a4b77d39568c3399d8485
huns <j840616@gmail.com>
2023-04-09 12:51:44 +0900

    9

--- slidigital/slidigital/settings.py
+++ slidigital/slidigital/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-apfv1@)#wp5og8#_b=dk_ro95jc&8%d7-0b9e=v^x)7=el7h@i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://hunki.pythonanywhere.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hunki.pythonanywhere.com']
 
 
 # Application definition
