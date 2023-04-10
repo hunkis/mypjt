4fa803dd0b6fbd4326f6b77ff0078ffc1ddf79e5
huns <j840616@gmail.com>
2023-04-11 00:24:57 +0900

    Commit message

--- test.py
+++ test.py
@@ -9,13 +9,6 @@
 # st.text('asdasd')
 
 
-## 1. 서버접속 (접속용 터미널)
-# cloud-sql-proxy.exe slidigital:us-central1:slidigital
-
-## 2. 원래 터미널
-# set GOOGLE_CLOUD_PROJECT=slidigital
-# set USE_CLOUD_SQL_AUTH_PROXY=true
-
 ## 3. django설정
 # python manage.py makemigrations
 # python manage.py makemigrations polls
@@ -24,15 +17,6 @@
 
 ## 4. 웹서버 실행
 # python manage.py runserver 8080
-
-## 5. 앱 배포
-# gcloud app deploy
-
-## 6. 배포 앱 실행
-# gcloud app browse
-
-## sql 서버 로그인 (x)
-# gcloud auth application-default login
 
 ## django 업데이트  
 # python manage.py makemigrations randing
