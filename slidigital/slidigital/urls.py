4dd7afa814bb3cd96ea4a917eb5ec80ffafe8a7b
huns <j840616@gmail.com>
2023-04-11 00:43:00 +0900

    411

--- slidigital/slidigital/urls.py
+++ slidigital/slidigital/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# import statistics
+# from django.conf import settings
 from django.urls import include, path
 from django.shortcuts import redirect
 from django.contrib import admin
@@ -25,6 +27,8 @@ urlpatterns = [
     path('statics/', include('randing.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# urlpatterns += statistics(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
